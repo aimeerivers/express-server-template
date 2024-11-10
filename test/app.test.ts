a8d624f4ac2814208e4881148833c9d1db3a5dfd
aimee rivers <aimee@aimeerivers.com>
2024-11-10 17:47:34 +0100

    test: Set up some simple tests for what we have so far

---
+++ test/app.test.ts
@@ -0,0 +1,33 @@
+import assert from "node:assert";
+import type { Server } from "node:http";
+import { after, before, describe, it } from "node:test";
+
+import request from "supertest";
+
+import { app } from "../src/app.js";
+
+const port = 18080;
+let server: Server;
+describe("App", () => {
+  before(() => {
+    server = app.listen(port);
+  });
+
+  describe("Home", () => {
+    it("should show the home page", async () => {
+      const res = await request(app).get("/");
+      assert.ok(res.text.includes("Welcome!"));
+    });
+  });
+
+  describe("Ping", () => {
+    it("should respond to a ping", async () => {
+      const res = await request(app).get("/ping");
+      assert.equal(res.statusCode, 200);
+    });
+  });
+
+  after(async () => {
+    server.close();
+  });
+});
